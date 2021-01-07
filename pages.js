3346bfd7ba4c612be2908ad32b7ea9e4b80c0cab
Bobby Jones Jr <jones9682@yahoo.com>
2021-01-07 14:14:41 -0500

    updated repoURL

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/jones9682/jones9682.github.io.git";
 
 ghpages.publish(
   pathname,
