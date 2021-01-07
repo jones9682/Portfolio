44728990941c98c3dcef54ef5920f4057c31f7a2
Bobby Jones Jr <jones9682@yahoo.com>
2021-01-07 14:48:10 -0500

    updated info/added resume and image

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,31 +6,31 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Robert",
   middleName: "",
-  lastName: "Shoaib",
+  lastName: "Jones jr.",
   message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/jones9682",
     },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
+    // {
+    //   image: "fa-facebook",
+    //   url: "https://www.facebook.com/",
+    // },
+    // {
+    //   image: "fa-instagram",
+    //   url: "https://www.instagram.com/",
+    // },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/robertjonesjr82",
     },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
+    // {
+    //   image: "fa-twitter",
+    //   url: "https://www.twitter.com/",
+    // },
   ],
 };
 
@@ -54,7 +54,7 @@ const about = {
   imageLink: require("../editable-stuff/hashirshoaeb.png"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
+    "My name is Robert Jones Jr. I grew up in New Jersey. I've always had a passion for technology and I am Self-motivated Entry Level Full Stack Web Developer with a background in Maintenance, Project Management, and life-long dedication to learning. Motivated towards game-design and showing my ability to problem solve. Experience in providing a high level of customer service to clients and customers while keeping a consistent work ethic. Passionate about designing and developing web applications utilizing skills in JavaScript, HTML5, API’s, jQuery as well maintaining the functionality of the website. Excited to leverage my skills in the future as part of a fast-paced, quality-driven team in order to build better experiences on the web. During My free time I enjoy fishing, camping, playing video games and spending quality time with my family.",
   resume: require("../editable-stuff/resume.pdf"),
 };
 
@@ -66,9 +66,9 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
+  gitHubUsername: "jones9682", //i.e."johnDoe12Gh"
   reposLength: 4,
-  specificRepos: [],
+  specificRepos: ["Budget-Tracker", "Eat-Da-Burger", "Password-Generator", "Employee-Tracker"],
 };
 
 // Leadership SECTION
@@ -100,14 +100,14 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
+    { name: "ORM", value: 60 },
     { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
+    { name: "Node.js", value: 70 },
+    { name: "JQuery", value: 65 },
+    { name: "JavaScript", value: 60 },
     { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "HTML/CSS", value: 80 },
+    { name: "REST APIs", value: 80 },
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
@@ -126,8 +126,8 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for full-time Software Engineering! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "rjonesjr2891@gmail.com",
 };
 
 // Blog SECTION
