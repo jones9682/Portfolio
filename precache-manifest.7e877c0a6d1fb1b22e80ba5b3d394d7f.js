0ecec147a8a97cfb5e6a585504a1668f4f214b94
Bobby Jones Jr <jones9682@yahoo.com>
2021-01-07 21:01:53 -0500

    Updates

---
+++ precache-manifest.7e877c0a6d1fb1b22e80ba5b3d394d7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af076e35e5496ff266c7dcc5e2e0aa7a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "492587f60e55f2a83a80",
+    "url": "/Portfolio/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/Portfolio/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Portfolio/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492587f60e55f2a83a80",
+    "url": "/Portfolio/static/js/main.9e36a836.chunk.js"
+  },
+  {
+    "revision": "87bcb24a22a073d38476",
+    "url": "/Portfolio/static/js/runtime-main.d4446722.js"
+  },
+  {
+    "revision": "69854b617168ba354c48f4af9f41407e",
+    "url": "/Portfolio/static/media/resume.69854b61.pdf"
+  },
+  {
+    "revision": "21501cafaa28460c424d879d08037fd1",
+    "url": "/Portfolio/static/media/rjonesjr.21501caf.png"
+  }
+]);
